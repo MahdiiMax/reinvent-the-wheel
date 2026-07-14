80852098179f4d40fdb97f2aacb06f2d1122c70f
MahdiiMax <mahdisadeghi.max@gmail.com>
2026-07-14 23:09:22 +0330

    feat: implement reflection based container

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,15 +1,22 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Controllers;
 
+use App\Services\UserService;
 use Core\Http\Response;
 
 class HomeController
 {
+    public function __construct(
+        private UserService $service
+    ) {
+    }
+
+
     public function index(): Response
     {
-        return new Response('Hello from Home Controller');
+        return new Response(
+            $this->service->hello()
+        );
     }
 }
--- core/Container/Container.php
+++ core/Container/Container.php
@@ -4,10 +4,29 @@ declare(strict_types=1);
 
 namespace Core\Container;
 
+use ReflectionClass;
+use ReflectionNamedType;
+
 class Container
 {
     public function make(string $class): object
     {
-        return new $class();
+        $reflection = new ReflectionClass($class);
+        $constructor = $reflection->getConstructor();
+        if (!$constructor) {
+            return new $class();
+        }
+        $dependencies = [];
+        foreach ($constructor->getParameters() as $parameter) {
+            $type = $parameter->getType();
+            if (!$type instanceof ReflectionNamedType) {
+                throw new \Exception("Cannot resolve dependency");
+            }
+            $dependencies[] = $this->make(
+                $type->getName()
+            );
+        }
+
+        return $reflection->newInstanceArgs($dependencies);
     }
 }
